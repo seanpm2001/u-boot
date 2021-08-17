353d39c402539d2ecd29344d6f38ce2254898321
Samuel Holland <samuel@sholland.org>
2021-08-16 22:08:20 -0500

    phy: sun4i-usb: Use DM_GPIO for id/vbus_det GPIOs
    Now that the sunxi_gpio driver handles pull-up/down via the driver
    model, we can switch to DM_GPIO for these pins with no loss in
    functionality. Since the driver now gets its pin configuration from
    the device tree, we can remove the Kconfig symbols.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/phy/allwinner/phy-sun4i-usb.c
+++ drivers/phy/allwinner/phy-sun4i-usb.c
@@ -96,32 +96,8 @@ struct sun4i_usb_phy_cfg {
 	int missing_phys;
 };
 
-struct sun4i_usb_phy_info {
-	const char *gpio_vbus_det;
-	const char *gpio_id_det;
-} phy_info[] = {
-	{
-		.gpio_vbus_det = CONFIG_USB0_VBUS_DET,
-		.gpio_id_det = CONFIG_USB0_ID_DET,
-	},
-	{
-		.gpio_vbus_det = NULL,
-		.gpio_id_det = NULL,
-	},
-	{
-		.gpio_vbus_det = NULL,
-		.gpio_id_det = NULL,
-	},
-	{
-		.gpio_vbus_det = NULL,
-		.gpio_id_det = NULL,
-	},
-};
-
 struct sun4i_usb_phy_plat {
 	void __iomem *pmu;
-	int gpio_vbus_det;
-	int gpio_id_det;
 	struct clk clocks;
 	struct reset_ctl resets;
 	struct udevice *vbus;
@@ -452,7 +428,6 @@ static int sun4i_usb_phy_probe(struct udevice *dev)
 	data->usb_phy = plat;
 	for (i = 0; i < data->cfg->num_phys; i++) {
 		struct sun4i_usb_phy_plat *phy = &plat[i];
-		struct sun4i_usb_phy_info *info = &phy_info[i];
 		char name[20];
 
 		if (data->cfg->missing_phys & BIT(i))
@@ -465,27 +440,6 @@ static int sun4i_usb_phy_probe(struct udevice *dev)
 			/* Fixed regulators cannot be disabled. */
 			if (ret && ret != -ENOSYS)
 				return ret;
-		}
-
-		phy->gpio_vbus_det = sunxi_name_to_gpio(info->gpio_vbus_det);
-		if (phy->gpio_vbus_det >= 0) {
-			ret = gpio_request(phy->gpio_vbus_det, "usb_vbus_det");
-			if (ret)
-				return ret;
-			ret = gpio_direction_input(phy->gpio_vbus_det);
-			if (ret)
-				return ret;
-		}
-
-		phy->gpio_id_det = sunxi_name_to_gpio(info->gpio_id_det);
-		if (phy->gpio_id_det >= 0) {
-			ret = gpio_request(phy->gpio_id_det, "usb_id_det");
-			if (ret)
-				return ret;
-			ret = gpio_direction_input(phy->gpio_id_det);
-			if (ret)
-				return ret;
-			sunxi_gpio_set_pull(phy->gpio_id_det, SUNXI_GPIO_PULL_UP);
 		}
 
 		if (data->cfg->dedicated_clocks)
