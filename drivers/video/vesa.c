80da15c6bf282ecc329f71302680751e1f3b02e6
Matheus Castello <matheus@castello.eng.br>
2024-05-06 13:46:20 -0300

    WIP: vesa: Use generic PCI command instead mttr
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- drivers/video/vesa.c
+++ drivers/video/vesa.c
@@ -9,13 +9,28 @@
 #include <pci.h>
 #include <vesa.h>
 #include <video.h>
-#include <asm/mtrr.h>
+
+#define VIRTIO_PCI_VENDOR_ID	0x1af4
+#define VIRTIO_PCI_DEVICE_ID10	0x1050
+
+static const struct udevice_id vesa_video_ids[] = {
+	{ .compatible = "vesa-fb" },
+	{ }
+};
+
+static struct pci_device_id vesa_video_supported[] = {
+	{ PCI_DEVICE_CLASS(PCI_CLASS_DISPLAY_VGA << 8, ~0) },
+	{ PCI_DEVICE(VIRTIO_PCI_VENDOR_ID, VIRTIO_PCI_DEVICE_ID10) },
+	{ },
+};
 
 static int vesa_video_probe(struct udevice *dev)
 {
 	struct video_uc_plat *plat = dev_get_uclass_plat(dev);
 	ulong fbbase;
 	int ret;
+	uint i;
+	pci_dev_t devno;
 
 	ret = vesa_setup_video(dev, NULL);
 	if (ret)
@@ -23,7 +38,10 @@ static int vesa_video_probe(struct udevice *dev)
 
 	/* Use write-combining for the graphics memory, 256MB */
 	fbbase = IS_ENABLED(CONFIG_VIDEO_COPY) ? plat->copy_base : plat->base;
-	mtrr_set_next_var(MTRR_TYPE_WRCOMB, fbbase, 256 << 20);
+
+	for (i = 0; (devno = pci_find_devices(vesa_video_supported, i)) >= 0; i++) {
+		pci_write_config_dword(devno, fbbase, 256 << 20);
+	}
 
 	return 0;
 }
@@ -39,22 +57,12 @@ static int vesa_video_bind(struct udevice *dev)
 	return 0;
 }
 
-static const struct udevice_id vesa_video_ids[] = {
-	{ .compatible = "vesa-fb" },
-	{ }
-};
-
 U_BOOT_DRIVER(vesa_video) = {
 	.name	= "vesa_video",
 	.id	= UCLASS_VIDEO,
 	.of_match = vesa_video_ids,
 	.bind	= vesa_video_bind,
 	.probe	= vesa_video_probe,
-};
-
-static struct pci_device_id vesa_video_supported[] = {
-	{ PCI_DEVICE_CLASS(PCI_CLASS_DISPLAY_VGA << 8, ~0) },
-	{ },
 };
 
 U_BOOT_PCI_DEVICE(vesa_video, vesa_video_supported);
